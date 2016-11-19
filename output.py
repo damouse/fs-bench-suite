a41995efd07e51e1f41b67dae6e1766613eee25c
damouse <damouse007@gmail.com>
2016-11-19 16:38:54 -0600

    aggregate graphs

--- output.py
+++ output.py
@@ -41,13 +41,11 @@ class TestResults():
         split = self.name.split('-')
         self.clients = int(split[0].replace('c', ''))
         self.requests = int(split[1].replace('r', ''))
-
-        # Pretty formatted names
-        self.save_name = self.fs + '-' + self.name
-        self.graph_name = '{} on {}: {} clients with {} requests each'.format('Apache' if test == 'apache' else 'Go Webserver', fs, self.clients, self.requests)
+        self.pretty_name = 'Go Webserver'
 
         if self.test is 'apache':
             self.unique = split[2]
+            self.pretty_name = 'Apache'
 
         # Load the data
         with open(filepath, 'r') as f:
@@ -77,58 +75,70 @@ def latency_scatter(result, output_subfolder=None):
     plot.xlim([0, result.end - result.start])
     plot.ylim([0, result.max_value])
 
-    # plot.title('')
+    plot.title('{} on {}: {} clients making {} requests'.format(result.pretty_name, result.fs, result.clients, result.requests))
     plot.ylabel('Latency (ms)')
     plot.xlabel('Request Send Time (ms from start)')
 
     if output_subfolder is None:
         plot.show()
     else:
-        plot.savefig(os.path.join(GRAPH_PATH, output_subfolder, result.save_name))
+        plot.savefig(os.path.join(output_subfolder, result.fs + '-' + result.name))
 
     plot.clf()
 
 
 def latency_cdf(result, output_subfolder=None):
     latencies = map(lambda x: x.diff_time, result.lines)
-
     cdfx = numpy.sort(latencies)
     cdfy = numpy.linspace(1 / len(latencies), 1.0, len(latencies))
     logcdfy = [-math.log10(1.0 - (float(idx) / len(latencies))) for idx in range(len(latencies))]
 
-    # plot.subplot(2, 2, 1)
     plot.plot(cdfx, logcdfy)
-    plot.title(result.graph_name)
-    plot.ylabel('Count')
-    plot.xlabel('Latency (ms)')
-    # plot.legend(loc='upper left')
-
-    # print result.end
-
     plot.xlim([0, result.max_value])
     plot.ylim([0, 1])
     plot.grid()
 
+    plot.title('{} on {}: {} clients making {} requests'.format(result.pretty_name, result.fs, result.clients, result.requests))
+    plot.ylabel('Count')
+    plot.xlabel('Latency (ms)')
+
     if output_subfolder is None:
         plot.show()
     else:
-        plot.savefig(os.path.join(GRAPH_PATH, output_subfolder, result.save_name))
+        plot.savefig(os.path.join(output_subfolder, result.fs + '-' + result.name))
 
-    # sys.exit(0)
     plot.clf()
 
 
-def samples():
-    with open('results/ext4/go-pg/10c-10r', 'r') as f:
-        lines = [x.split(',') for x in f.readlines()]
+def aggregate_cdf(results, output_subfolder=None):
+    # Create 3-tuples that contain the same test conditions for each fs
+    for test_group in zip(*[filter(lambda x: x.fs == fs, results) for fs in ['ntfs', 'ext4', 'zfs']]):
+        for fs in test_group:
+            latencies = map(lambda x: x.diff_time, fs.lines)
+            cdfx = numpy.sort(latencies)
+            cdfy = numpy.linspace(1 / len(latencies), 1.0, len(latencies))
+            logcdfy = [-math.log10(1.0 - (float(idx) / len(latencies))) for idx in range(len(latencies))]
+            plot.plot(cdfx, logcdfy, label=fs.fs)
 
-    sorted(lines, key=lambda x: x[1])
-    start = int(lines[0][1])
-    end = int(lines[-1][1])
-    max_value = int(max(lines, key=lambda x: int(x[3]))[3])
-    times = [int(x[1]) - start for x in lines]
-    latencies = [int(x[3]) for x in lines]
-    data = latencies
+        plot.ylabel('Count')
+        plot.xlabel('Latency (ms)')
+        plot.title('{} Duration CDF: {} clients with {} requests each'.format(test_group[0].pretty_name, test_group[0].clients, test_group[0].requests))
+        plot.legend()
+
+        plot.xlim([0, max(map(lambda x: x.max_value, test_group)) * 2])
+        plot.ylim([0, 5])
+        plot.grid()
+        # plot.show()
+        # sys.exit(0)
+
+        if output_subfolder is None:
+            plot.show()
+        elif test_group[0].test == 'apache':
+            plot.savefig(os.path.join(output_subfolder, '{}-{}-{}'.format(test_group[0].clients, test_group[0].requests, test_group[0].unique)))
+        else:
+            plot.savefig(os.path.join(output_subfolder, '{}-{}'.format(test_group[0].clients, test_group[0].requests)))
+
+        plot.clf()
 
 
 def pdf(x, mu=0, sigma=1):
@@ -137,13 +147,25 @@ def pdf(x, mu=0, sigma=1):
     return term1 * term2
 
 if __name__ == '__main__':
-    data = load_data()
+    all_data = load_data()
 
-    # runner.cleardir(os.path.join(GRAPH_PATH, 'scatter'))
-    # [latency_scatter(d, "scatter") for d in data]
+    for test in ['apache', 'go-pg']:
+        data = filter(lambda x: x.test == test, all_data)
 
-    runner.cleardir(os.path.join(GRAPH_PATH, 'cdf'))
-    [latency_cdf(d, "cdf") for d in data]
+        # Individual scatter plots
+        # p = os.path.join(GRAPH_PATH, test, 'scatter')
+        # runner.cleardir(p)
+        # [latency_scatter(d, p) for d in data]
+
+        # # Individual CDFs
+        # p = os.path.join(GRAPH_PATH, test, 'cdf')
+        # runner.cleardir(p)
+        # [latency_cdf(d, p) for d in data]
+
+        # Aggregate CDFs
+        p = os.path.join(GRAPH_PATH, test, 'aggregate-cdf')
+        runner.cleardir(p)
+        aggregate_cdf(data, p)
 
     # print data[0].name
     # latency_scatter(data[0])
