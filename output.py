4495eb4f00ea6e05813c8b9be1c2a21d49abf2a5
damouse <damouse007@gmail.com>
2016-12-17 14:01:16 -0600

    working on pdfs

--- output.py
+++ output.py
@@ -7,8 +7,12 @@ import glob
 import os
 import sys
 
-import numpy
-import matplotlib.pyplot as plot
+import numpy as np
+from scipy.stats.kde import gaussian_kde
+import seaborn as sns
+
+from numpy import linspace
+import matplotlib.pyplot as plt
 from math import ceil, floor, sqrt
 from scipy import stats
 
@@ -18,6 +22,7 @@ import runner
 FILE_SEPERATOR = '\\' if os.name == 'nt' else '/'
 RESULTS_PATH = 'results'
 GRAPH_PATH = 'graphs'
+PDF_BINS = 50
 
 
 class ResultRow():
@@ -48,10 +53,6 @@ class MacroResults():
         self.requests = int(split[1].replace('r', ''))
         self.pretty_name = 'Apache' if self.test is 'apache' else 'Go Webserver'
 
-        # if self.test is 'apache':
-        #     self.unique = split[2]
-        # self.pretty_name = 'Apache'
-
         # Load the data
         with open(filepath, 'r') as f:
             self.lines = sorted([ResultRow(x) for x in f.readlines()], key=lambda x: x.start_time)
@@ -67,106 +68,89 @@ def latency_scatter(result, output_subfolder):
     time = [x.start_time - result.start for x in result.lines]
     lat = [x.diff_time for x in result.lines]
 
-    plot.scatter(time, lat, label="N=" + " M=")
-    plot.xlim([0, result.end - result.start])
-    plot.ylim([0, result.max_value])
+    plt.scatter(time, lat, label="N=" + " M=")
+    plt.xlim([0, result.end - result.start])
+    plt.ylim([0, result.max_value])
 
-    plot.title('{} clients, {}, {}, {}s'.format(result.clients, result.pretty_name, result.fs, result.requests))
-    plot.ylabel('Latency (ms)')
-    plot.xlabel('Time (ms)')
+    plt.title('{} clients, {}, {}, {}s'.format(result.clients, result.pretty_name, result.fs, result.requests))
+    plt.ylabel('Latency (ms)')
+    plt.xlabel('Time (ms)')
 
-    show_or_save(result, output_subfolder)
-    plot.clf()
+    plt.savefig(os.path.join(output_subfolder, result.fs + '-' + result.name))
+    plt.clf()
 
 
 def latency_cdf(result, output_subfolder):
-    data = numpy.sort(map(lambda x: x.diff_time, result.lines))
+    data = np.sort(map(lambda x: x.diff_time, result.lines))
 
-    plot.plot(data, numpy.linspace(0, 1, len(data)))
-    plot.xlim([0, result.max_value])
-    plot.ylim([0, 1])
-    plot.grid()
+    plot_pdf(data)
 
-    plot.title('{} clients, {}, {}, {}s'.format(result.clients, result.pretty_name, result.fs, result.requests))
-    plot.ylabel('Count')
-    plot.xlabel('Latency (ms)')
+    plt.xlim([0, result.max_value])
+    plt.ylim([0, 1])
+    plt.grid()
 
-    show_or_save(result, output_subfolder)
-    plot.clf()
+    plt.title('{} clients, {}, {}, {}s'.format(result.clients, result.pretty_name, result.fs, result.requests))
+    plt.ylabel('Count')
+    plt.xlabel('Latency (ms)')
+
+    plt.savefig(os.path.join(output_subfolder, result.fs + '-' + result.name))
+    plt.clf()
 
 
 def latency_boxplot(result, output_subfolder=None):
-    bins = numpy.linspace(result.start, result.end, 30)
+    bins = np.linspace(result.start, result.end, 30)
     binned = [filter(lambda x: maxx > x.start_time > minn, result.lines) for (minn, maxx) in zip(bins[:-1], bins[1:])]
     binned = [map(lambda x: x.diff_time, y) for y in binned]
 
-    plot.boxplot(binned, 0, '')
-    plot.ylabel('Request Latency (ms)')
-    plot.xlabel('Time (s)')
-    plot.title('Boxed latency for {} clients, {}, {}s'.format(result.clients, result.pretty_name, result.requests))
-    show_or_save(result, output_subfolder)
-    plot.clf()
+    plt.boxplot(binned, 0, '')
+    plt.ylabel('Request Latency (ms)')
+    plt.xlabel('Time (s)')
+    plt.title('Boxed latency for {} clients, {}, {}s'.format(result.clients, result.pretty_name, result.requests))
+
+    plt.savefig(os.path.join(output_subfolder, result.fs + '-' + result.name))
+    plt.clf()
 
 
-def aggregate_boxplot(results, output_subfolder):
-    for test_group in zip(*[filter(lambda x: x.fs == fs, results) for fs in ['ntfs', 'ext4', 'zfs']]):
-        for fs in test_group:
-            bins = numpy.linspace(fs.start, fs.end, 30)
-            binned = [filter(lambda x: maxx > x.start_time > minn, fs.lines) for (minn, maxx) in zip(bins[:-1], bins[1:])]
-            binned = [map(lambda x: x.diff_time, y) for y in binned]
-            medians = [numpy.median(x) for x in binned]
+def plot_pdf(data, bins=PDF_BINS, label=None):
+    sns.kdeplot(np.array(data))
+    return
+    # Plots a pdf but doesnt save it
+    plt.subplot(211)
+    hist, bins = np.histogram(data, bins=bins, density=False)
+    bin_centers = (bins[1:] + bins[:-1]) * 0.5
+    avg = [float(x) / float(len(data)) for x in hist]
+    plt.plot(bin_centers, avg)
 
-            line = plot.plot([0] + medians, label=fs.fs)
-            color = line[0].get_color()
-            boxes = plot.boxplot(binned, 0, '')
-
-            plot.setp(boxes['boxes'], color=line[0].get_color())
-            plot.setp(boxes['whiskers'], color=color)
-            plot.setp(boxes['caps'], color=color)
-            plot.setp(boxes['fliers'], color=color, marker='o')
-            plot.setp(boxes['medians'], color=color)
-
-        plot.ylabel('Request Latency (ms)')
-        plot.xlabel('Time (s)')
-        plot.title('Boxed latency for {} clients, {}, {}s'.format(test_group[0].clients, test_group[0].pretty_name, test_group[0].requests))
-        plot.legend(loc='upper left')
-
-        if test_group[0].clients == 40 and test_group[0].test == 'apache':
-            plot.ylim([02, 200])
-
-        plot.grid()
-
-        # if test_group[0].test == 'apache':
-        #     plot.savefig(os.path.join(output_subfolder, '{}-{}-{}'.format(test_group[0].clients, test_group[0].requests, test_group[0].unique)))
-        # else:
-        plot.savefig(os.path.join(output_subfolder, '{}-{}'.format(test_group[0].clients, test_group[0].requests)))
-
-        plot.clf()
+    kde = gaussian_kde(data)
+    dist_space = linspace(min(data), max(data), 500)
+    plt.subplot(212)
+    plt.plot(dist_space, kde(dist_space), label=label)
 
 
 def aggregate_client_cdf(results, output_subfolder):
     for test_group in zip(*[filter(lambda x: x.fs == fs, results) for fs in ['ntfs', 'ext4', 'zfs']]):
         for fs in test_group:
-            latencies = map(lambda x: x.diff_time, fs.lines)
-            cdfx = numpy.sort(latencies)
-            plot.plot(cdfx, numpy.linspace(0, 1, len(cdfx)), label=fs.fs)
+            data = map(lambda x: x.diff_time, fs.lines)
 
-        plot.ylabel('Count (%)')
-        plot.xlabel('Request Latency (ms)')
-        plot.title('CDF for {} clients, {}, {}s'.format(test_group[0].clients, test_group[0].pretty_name, test_group[0].requests))
-        plot.legend(loc='lower right')
+            # hist, bins = np.histogram(data, bins=PDF_BINS, density=False)
+            # bin_centers = (bins[1:] + bins[:-1]) * 0.5
+            # avg = [float(x) / float(len(data)) for x in hist]
+            # plt.plot(bin_centers, avg, label=fs.fs)
 
-        plot.xlim([0, max(map(lambda x: x.max_value, test_group))])
-        # plot.xlim([0, 100])
-        plot.ylim([0, 1.05])
-        plot.grid()
+            plot_pdf(data, label=fs.fs)
 
-        # if test_group[0].test == 'apache':
-        #     plot.savefig(os.path.join(output_subfolder, '{}-{}-{}'.format(test_group[0].clients, test_group[0].requests, test_group[0].unique)))
-        # else:
-        plot.savefig(os.path.join(output_subfolder, '{}-{}'.format(test_group[0].clients, test_group[0].requests)))
+        plt.ylabel('Count (%)')
+        plt.xlabel('Request Latency (ms)')
+        plt.title('CDF for {} clients, {}, {}s'.format(test_group[0].clients, test_group[0].pretty_name, test_group[0].requests))
+        plt.legend(loc='lower right')
 
-        plot.clf()
+        plt.xlim([0, max(map(lambda x: x.max_value, test_group))])
+        # plt.xlim([0, 100])
+        plt.ylim([0, 1.05])
+
+        plt.grid()
+        plt.savefig(os.path.join(output_subfolder, '{}-{}'.format(test_group[0].clients, test_group[0].requests)))
+        plt.clf()
 
 
 def aggregate_platform_cdf(results, output_subfolder):
@@ -174,37 +158,31 @@ def aggregate_platform_cdf(results, output_subfolder):
 
     for fs_slice in [filter(lambda x: x.fs == fs, results) for fs in ['ntfs', 'ext4', 'zfs']]:
         for test_slice in [filter(lambda x: x.test == test, fs_slice) for test in ['go-pg', 'apache']]:
-            if len(test_slice) == 0:
-                continue
-            tests.append(test_slice)
-
-            # if test_slice[0].test == 'apache':
-            #     tests.append(filter(lambda x: 'unique' in x.name, test_slice))
-            #     tests.append(filter(lambda x: 'unique' not in x.name, test_slice))
-            # else:
-            #     tests.append(test_slice)
+            if len(test_slice) != 0:
+                tests.append(test_slice)
 
     for test_group in tests:
         for fs in sorted(test_group, key=lambda x: x.clients):
-            latencies = map(lambda x: x.diff_time, fs.lines)
-            cdfx = numpy.sort(latencies)
-            plot.plot(cdfx, numpy.linspace(0, 1, len(cdfx)), label=str(fs.clients) + " clients")
+            data = map(lambda x: x.diff_time, fs.lines)
 
-        plot.ylabel('Count (%)')
-        plot.xlabel('Latency (ms)')
-        plot.title('CDF for varying Clients, {} on {}, {}s'.format(test_group[0].pretty_name, test_group[0].fs, test_group[0].requests))
-        plot.legend(loc='lower right')
+            # hist, bins = np.histogram(data, bins=PDF_BINS, density=False)
+            # bin_centers = (bins[1:] + bins[:-1]) * 0.5
+            # avg = [float(x) / float(len(data)) for x in hist]
+            # plt.plot(bin_centers, avg, label=str(fs.clients) + " clients")
 
-        # plot.xlim([0, 100])
-        plot.ylim([0, 1.05])
-        plot.grid()
+            plot_pdf(data, label=str(fs.clients) + " clients")
 
-        # if test_group[0].test == 'apache':
-        #     plot.savefig(os.path.join(output_subfolder, '{}-{}'.format(test_group[0].fs, test_group[0].unique)))
-        # else:
-        plot.savefig(os.path.join(output_subfolder, '{}'.format(test_group[0].fs)))
+        plt.ylabel('Count (%)')
+        plt.xlabel('Latency (ms)')
+        plt.title('CDF for varying Clients, {} on {}, {}s'.format(test_group[0].pretty_name, test_group[0].fs, test_group[0].requests))
+        plt.legend(loc='lower right')
 
-        plot.clf()
+        # plt.xlim([0, 100])
+        plt.ylim([0, 1.05])
+
+        plt.grid()
+        plt.savefig(os.path.join(output_subfolder, '{}'.format(test_group[0].fs)))
+        plt.clf()
 
 
 def load_macrobenchmarks():
@@ -214,57 +192,59 @@ def load_macrobenchmarks():
             for file in glob.glob(os.path.join(RESULTS_PATH, fs, test, '*'))]
 
 
-def load_microbenchmarks():
-    return [MicroResults(fs, test, file)
-            for test in ['microbenchmarks']
-            for fs in ['ntfs', 'ext4', 'zfs']
-            for file in glob.glob(os.path.join(RESULTS_PATH, fs, test, '*'))]
-
-
-def show_or_save(result, output_subfolder):
-    if output_subfolder is None:
-        plot.show()
-    else:
-        plot.savefig(os.path.join(output_subfolder, result.fs + '-' + result.name))
-
-    plot.clf()
-
-
 def graph(all_data):
     for test in ['apache', 'go-pg']:
         data = filter(lambda x: x.test == test, all_data)
 
-        # Individual scatter plots
-        p = os.path.join(GRAPH_PATH, test, 'scatter')
+        # # Individual scatter plots
+        # p = os.path.join(GRAPH_PATH, test, 'scatter')
+        # runner.cleardir(p)
+        # [latency_scatter(d, p) for d in data]
+
+        # Individual CDFs
+        p = os.path.join(GRAPH_PATH, test, 'cdf')
         runner.cleardir(p)
-        [latency_scatter(d, p) for d in data]
+        [latency_cdf(d, p) for d in data]
 
-        # # Individual CDFs
-        # p = os.path.join(GRAPH_PATH, test, 'cdf')
-        # runner.cleardir(p)
-        # [latency_cdf(d, p) for d in data]
+        # Aggregate CDFs per # clients
+        p = os.path.join(GRAPH_PATH, test, 'aggregate-client-cdf')
+        runner.cleardir(p)
+        aggregate_client_cdf(data, p)
 
-        # # Aggregate CDFs per # clients
-        # p = os.path.join(GRAPH_PATH, test, 'aggregate-client-cdf')
-        # runner.cleardir(p)
-        # aggregate_client_cdf(data, p)
+        # Aggregate CDFs per fs
+        p = os.path.join(GRAPH_PATH, test, 'aggregate-fs-cdf')
+        runner.cleardir(p)
+        aggregate_platform_cdf(data, p)
 
-        # # Aggregate CDFs per fs
-        # p = os.path.join(GRAPH_PATH, test, 'aggregate-fs-cdf')
-        # runner.cleardir(p)
-        # aggregate_platform_cdf(data, p)
 
-        # # Aggregate Boxplotss
-        # p = os.path.join(GRAPH_PATH, test, 'aggregate-boxplot')
-        # runner.cleardir(p)
-        # aggregate_boxplot(data, p)
+def test_pdf():
+    # Design note: this is an excellent API because it needs the minimum amount of information to dive down the tree
+    # without corralling it all together into a full filename (minus requiring the whole name at the end,
+    # that was immensely stupid.
+    result = MacroResults('zfs', 'apache', 'results/zfs/apache/10c-30r')
+    data = np.array([x.diff_time for x in result.lines])
 
-        # # Individual Boxplots
-        # p = os.path.join(GRAPH_PATH, test, 'boxplot')
-        # runner.cleardir(p)
-        # [latency_boxplot(d, p) for d in data]
+    # Method 1
+    plt.subplot(311)
+    hist, bins = np.histogram(data, bins=400, density=False)
+    bin_centers = (bins[1:] + bins[:-1]) * 0.5
+    avg = [float(x) / float(len(data)) for x in hist]
+    plt.plot(bin_centers, avg)
 
+    # Method 2
+    kde = gaussian_kde(data)
+    dist_space = linspace(min(data), max(data), 100)
+    plt.subplot(312)
+    plt.plot(dist_space, kde(dist_space))
+
+    # Method 3
+    plt.subplot(313)
+    # sns.set_style('whitegrid')
+    sns.kdeplot(np.array(data))
+
+    plt.show()
 
 if __name__ == '__main__':
     all_data = load_macrobenchmarks()
     graph(all_data)
+    # test_pdf()
